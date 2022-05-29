55e33a5c512e931516abad4dcf88fdc8fea52936
Veysel Aksin <veysel.aksn@gmail.com>
2022-05-30 00:17:45 +0300

    Fixed some bugs 🪲

--- app/pages/views.py
+++ app/pages/views.py
@@ -13,6 +13,9 @@ def index(request):
 
     features = features_list.FEATURES_LIST
 
+    if request.user.is_authenticated:
+        return redirect("home")
+
     context = {
         "hero_title": "Vector Job",
         "hero_text": "The job you've been looking for is here! If you are looking for a job, the vector job platform has been developed just for you. Don't wait, register now!",
--- app/users/urls.py
+++ app/users/urls.py
@@ -1,10 +1,16 @@
 from unicodedata import name
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register', views.register_page, name='register_page'),
     path('login', views.login_page, name='login_page'),
     path('logout', views.logout_user, name="logout_page"),
-    path('activate<uidb64>/<token>/', views.activate, name='activate')
+    path('activate<uidb64>/<token>/', views.activate, name='activate'),
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
